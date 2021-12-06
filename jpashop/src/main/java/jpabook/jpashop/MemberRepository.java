6b5ac9dba1f06d371e745ba941bf5860d6b7d991
hope1213 <1213hope@gmail.com>
2021-12-06 19:11:43 +0900

    JPA 설정, 로그, 회원 저장 테스트

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext //엔티티매니저 (jpa할때 필요)
+    EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId(); // member로 반환하면 되는데 왜 id만 반환..? 커멘드와 쿼리를 분리하라는 원칙에 의해 저장을 하면 커멘드 성이기 때문에 리터값을 잘 안만든가..? 여튼 아이디만 반환하면 나머지 조회 다할수잇어서..?
+    }
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.Member;
+import jpabook.jpashop.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // 엔티티매니저를 통한 모든 데이터 변경은 트렌젝션 안에서 이뤄져야함
+	@Rollback(false)	//테스트 종료 후 데이터를 롤백하지 않고 그대로 남겨두는 옵션
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("1hoon");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+//        assertEquals(findMember.getId(), member.getId());
+//        assertEquals(findMember.getUsername(), member.getUsername());
+//        assertEquals(findMember, member);
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+
+    }
+}
