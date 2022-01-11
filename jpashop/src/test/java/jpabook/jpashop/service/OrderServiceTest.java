542de14b840add08f28c323020e2a618d805411a
hope1213 <1213hope@gmail.com>
2022-01-12 00:10:11 +0900

    주문취소 테스트

--- jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -43,17 +43,25 @@ class OrderServiceTest {
         assertEquals(OrderStatus.ORDER, getOrder.getStatus());
         assertEquals(orderid, getOrder.getId());
         assertEquals(1, getOrder.getOrderItems().size()); // 주문한 상품 종류 수
-        assertEquals(10000 * orderCount, getOrder.getTotalPrice()); // 주문 가격
-        assertEquals(8, item.getStockQuantity());
+        assertEquals(10000 * orderCount, getOrder.getTotalPrice(),"주문한 가격이 같아야 한다."); // 주문 가격
+        assertEquals(8, item.getStockQuantity(),"주문하고 남은 상품의 개수");
     }
 
     @Test
     void 주문취소() throws Exception{
         //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(),
+                orderCount);
+        //When
+        orderService.cancelOrder(orderId);
 
-        //when
-
-        //then
+        //Then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals(OrderStatus.CANCEL, getOrder.getStatus(),"주문 취소시 상태는 CANCEL 이다.");
+        assertEquals( 10, item.getStockQuantity(),"주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
     }
 
     @Test
