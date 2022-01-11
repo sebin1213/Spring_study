8eee05ad68b9dfac12113a28f3cf97aee283264b
hope1213 <1213hope@gmail.com>
2022-01-12 02:41:30 +0900

    search 기능 추가 + 동적쿼리

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -1,2 +1,11 @@
-package jpabook.jpashop.domain;public class OrderSearch {
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class OrderSearch {
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus;//주문 상태[ORDER, CANCEL]
+//Getter, Setter
 }
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Order;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import jpabook.jpashop.domain.*;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.criteria.internal.predicate.BooleanExpressionPredicate;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -20,7 +25,71 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
 
-    //검색기능
-//    public List<Order> findAll(OrderSearch orderSearch){}
+
+
+    /**검색기능**/
+    //검색 조건에 동적으로 쿼리를 생성해서 주문 엔티티를 조회한다.
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            }
+            else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            }
+            else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch){
+//        QOrder order = QOrder.order;
+//        QMember member = QMember.member;
+//
+//        return query
+//                .select(order)
+//                .from(order)
+//                .join(order.member, member)
+//                .where(statusEq(orderSearch.getOrderStatus()),
+//                        nameLike(orderSearch.getMemberName()))
+//                .limit(1000)
+//                .fetch();
+//    }
+//    private BooleanExpression statusEq(OrderStatus statusCond){
+//        if(statusCond == null){
+//            return null;
+//        }
+//        return order.status.eq(statusCond);
+//    }
+//    private BooleanExpression nameLike(String nameCond){
+//        if (!StringUtils.hasText(nameCond)){
+//            return null;
+//        }
+//        return member.name.like(nameCond)
+//    }
 
 }
