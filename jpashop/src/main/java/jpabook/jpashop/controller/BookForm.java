fc401a1919828903a72b179ba41158683fadac9a
hope1213 <1213hope@gmail.com>
2022-01-12 18:49:02 +0900

    Form 추가

--- jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -1,2 +1,17 @@
-package jpabook.jpashop.controller;public class BookForm {
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id; // 상품수정을 할꺼라 id값이 있어야함
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+
 }
