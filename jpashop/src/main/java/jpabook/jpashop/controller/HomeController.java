afc6ff258e71f9be66ae6d8b6713e2d1911a498c
hope1213 <1213hope@gmail.com>
2022-01-12 18:49:26 +0900

    상품, 회원controller추가

--- jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,2 +1,15 @@
-package jpabook.jpashop.controller;public class HomeController {
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j // 로그남기는 롬복 어노테이션
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
 }
