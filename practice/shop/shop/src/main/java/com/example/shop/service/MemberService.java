986c82a7fe24bc92135374feb3e22d2858f69045
hope1213 <1213hope@gmail.com>
2022-01-25 20:51:26 +0900

    memberservice 추가

--- practice/shop/shop/src/main/java/com/example/shop/service/MemberService.java
+++ practice/shop/shop/src/main/java/com/example/shop/service/MemberService.java
@@ -1,2 +1,31 @@
-package com.example.shop.service;public class MemberService {
+package com.example.shop.service;
+
+import com.example.shop.domain.Member.Member;
+import com.example.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 아이디,이메일 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> findMemberUserId = memberRepository.findByUserId(member.getUserid());
+        if (!findMemberUserId.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 아이디입니다.");
+        }
+        Optional<Member> findMemberEmail = memberRepository.findByEmail(member.getUserid());
+        if (!findMemberEmail.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+        }
+    }
 }
