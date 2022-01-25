fd11c3ce80cefb5f3e2895f2a60ae730e1103496
hope1213 <1213hope@gmail.com>
2022-01-25 20:50:54 +0900

    컨트롤러, memberform 추가

--- practice/shop/shop/src/main/java/com/example/shop/controller/form/MemberForm.java
+++ practice/shop/shop/src/main/java/com/example/shop/controller/form/MemberForm.java
@@ -1,2 +1,35 @@
-package com.example.shop.controller.form;public class MemberForm {
+package com.example.shop.controller.form;
+
+import com.example.shop.domain.Address;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotBlank(message = "아이디는 필수 입니다")
+    @Size(min = 8, message = "아이디는 8자 이상이여야 합니다!")
+    private String userid;
+
+    @NotBlank(message = "비밀번호는 필수 입니다")
+    @Size(min = 10, message = "비밀번호는 10자 이상이여야 합니다!")
+    private String password;
+
+    @NotBlank(message = "회원 이름은 필수 입니다")
+    private String username;
+
+    @NotBlank(message = "이메일은 필수 입니다")
+    @Email
+    private String email;
+
+//    @NotBlank(message = "핸드폰 번호는 필수 입니다")
+//    private String phone;
 }
