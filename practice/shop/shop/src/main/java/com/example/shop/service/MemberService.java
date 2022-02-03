97ef417ffb5db153df2a422ea4f2957a23738dc8
hope1213 <1213hope@gmail.com>
2022-02-03 16:58:51 +0900

    이거때문에 오류 발생....공부하고 수정할것

--- practice/shop/shop/src/main/java/com/example/shop/service/MemberService.java
+++ practice/shop/shop/src/main/java/com/example/shop/service/MemberService.java
@@ -4,21 +4,33 @@ import com.example.shop.domain.Member.Member;
 import com.example.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
     public Long join(Member member){
-        validateDuplicateMember(member); // 중복 아이디,이메일 검증
+//        validateDuplicateMember(member); // 중복 아이디,이메일 검증
         memberRepository.save(member);
         return member.getId();
     }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+    public List<Member> findAll(Long memberId) {
+        return memberRepository.findAll();
+    }
+
+
     private void validateDuplicateMember(Member member) {
+        System.out.println(memberRepository.findByUserId(member.getUserid()));
         Optional<Member> findMemberUserId = memberRepository.findByUserId(member.getUserid());
         if (!findMemberUserId.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 아이디입니다.");
