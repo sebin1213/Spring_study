a1b0cd47a1b1ae206caa2d4b86c3268d84ba545c
hope1213 <1213hope@gmail.com>
2022-02-03 17:36:46 +0900

    default값 설정을 위해 @DynamicInsert, @DynamicUpdate 어노테이션을 사용하여 null이 아닌 컬럼만 불러오게 설정

--- practice/shop/shop/src/main/java/com/example/shop/domain/Member/Member.java
+++ practice/shop/shop/src/main/java/com/example/shop/domain/Member/Member.java
@@ -8,12 +8,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@DynamicInsert //하이버네이트는 insert나 update 시 모든 컬럼을 사용하지만 default값을 넣어주기 위해 null인 필드를 제외하는 어노테이션을 추가한다.
+@DynamicUpdate //update 시 null 인필드 제외
 @Getter @Setter
 @JsonIgnoreProperties(value={"password"})
 public class Member {
@@ -35,6 +39,8 @@ public class Member {
 
 //    @Column(unique = true)
     private String phone;
+
+    @ColumnDefault("0")
     private String point;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
