a237625e83c7ff5da3e8245a6ed833b505ab7021
hope1213 <1213hope@gmail.com>
2022-02-05 23:45:45 +0900

    이메일 인증 도메인

--- practice/shop/shop/src/main/java/com/example/shop/domain/Member/EmailAuth.java
+++ practice/shop/shop/src/main/java/com/example/shop/domain/Member/EmailAuth.java
@@ -1,2 +1,37 @@
-package com.example.shop.domain.Member;public class EmailAuth {
+package com.example.shop.domain.Member;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmailAuth {
+
+    private static final Long MAX_EXPIRE_TIME = 5L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String authToken;
+    private Boolean expired;
+    private LocalDateTime expireDate;
+
+//    @Builder
+//    public EmailAuth(String email, String authToken, Boolean expired) {
+//        this.email = email;
+//        this.authToken = authToken;
+//        this.expired = expired;
+//        this.expireDate = LocalDateTime.now().plusMinutes(MAX_EXPIRE_TIME); // 만료시간 5분으로 설정함
+//    }
+//
+//    public void useToken() {
+//        this.expired = true;
+//    }
 }
