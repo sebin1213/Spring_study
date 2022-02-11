c875b590109efcba5a1726576699158d140bc06e
hope1213 <1213hope@gmail.com>
2022-02-11 23:41:45 +0900

    repository jpa로 변경

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,2 +1,11 @@
-package jpabook.jpashop.repository;public class MemberRepository {
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+// 스프링 데이터는 JpaRepository 여기에 save, findById, saveall 등등 겁나 많음!
+// 구현체 만들필요없이 알아서 해줌
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByName(String name);
 }
