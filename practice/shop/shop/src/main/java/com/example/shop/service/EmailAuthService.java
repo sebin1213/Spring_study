8ee85a13432e841f44f085c0d3632d37d6183092
hope1213 <1213hope@gmail.com>
2022-02-17 23:27:32 +0900

    이메일 인증 로직

--- practice/shop/shop/src/main/java/com/example/shop/service/EmailAuthService.java
+++ practice/shop/shop/src/main/java/com/example/shop/service/EmailAuthService.java
@@ -2,6 +2,7 @@ package com.example.shop.service;
 
 import com.example.shop.repository.EmailAuthRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +11,12 @@ import org.springframework.stereotype.Service;
 public class EmailAuthService {
     private final EmailAuthRepository emailAuthRepository;
 
-    @Async
-    public void send(String email, String authToken) {
-        SimpleMailMessage smm = new SimpleMailMessage();
-        smm.setTo(email+"@gmail.com");
-        smm.setSubject("회원가입 이메일 인증");
-        smm.setText("http://localhost:8080/sign/confirm-email?email="+email+"&authToken="+authToken);
-        javaMailSender.send(smm);
-    }
+//    @Async
+//    public void send(String email, String authToken) {
+//        SimpleMailMessage smm = new SimpleMailMessage();
+//        smm.setTo(email+"@gmail.com");
+//        smm.setSubject("회원가입 이메일 인증");
+//        smm.setText("http://localhost:8080/sign/confirm-email?email="+email+"&authToken="+authToken);
+//        javaMailSender.send(smm);
+//    }
 }
