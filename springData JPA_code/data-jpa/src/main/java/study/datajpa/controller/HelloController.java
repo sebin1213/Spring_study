d3a32d97d5476b9fe291a0ca4d4c1bbc883bd15d
hope1213 <1213hope@gmail.com>
2022-02-28 23:48:54 +0900

    동작확인

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/controller/HelloController.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/controller/HelloController.java
@@ -1,2 +1,12 @@
-package study.datajpa.controller;public class HelloController {
+package study.datajpa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
