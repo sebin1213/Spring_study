73cb3960326b77e0e0cc4f342f2de0b0e8701e43
hope1213 <1213hope@gmail.com>
2022-03-01 18:31:45 +0900

    회원 스프링데이터 jpa사용한 레파지토리 테스트 추가

--- springData JPA_code/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ springData JPA_code/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -16,6 +20,8 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -51,4 +57,85 @@ class MemberRepositoryTest {
         long deletedCount = memberRepository.count();
         Assertions.assertThat(deletedCount).isEqualTo(0);
     }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<Member> result =
+                memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
+        Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<Member> result =
+                memberRepository.findUser("AAA", 15);
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
+        Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findUserNameList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<String> usernameList =
+                memberRepository.findUsernameList();
+        for (String s : usernameList){
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamaaa");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto =
+                memberRepository.findMemberDto();
+        for (MemberDto m : memberDto){
+            System.out.println("m = " + m);
+        }
+    }
+
+
+    @Test
+    public void findMemberList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<Member> usernameList = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for (Member s : usernameList){
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void returnType(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+//        Member aaa = memberRepository.findMemberByUsername("AAA"); //현재는 에러남 2개이상의 데이터를 넣었는데 하나만 출력하라고 해서..
+        List<Member> bbb = memberRepository.findListByUsername("AAAff");
+//        System.out.println("size???" + bbb.size()); // 사이즈가 0으로 나옴 존재하지 않아도 null로 반환함 이제는 그냥 optional로 사용함
+    }
+
+
 }
