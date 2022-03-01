041a1ba901df7b4905a23c114e784d5890d2c67d
hope1213 <1213hope@gmail.com>
2022-03-01 18:30:18 +0900

    회원 여러조건검색

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -34,4 +34,14 @@ public class MemberJpaRepository { //update는 따로 필요없음 (변경감지
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
+
+    public List<Member> findByUsernameAndAgeGreaterThan(String username, int age) {
+        return em.createQuery("select m from Member m where m.username = :username and m.age > :age")
+                .setParameter("username", username)
+                .setParameter("age", age)
+                .getResultList();
+    }
+
+
+
 }
