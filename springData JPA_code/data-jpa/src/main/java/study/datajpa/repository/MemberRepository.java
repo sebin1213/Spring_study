bbf0ab83c2245fff3e293a3f2bee2a7c748a6c98
hope1213 <1213hope@gmail.com>
2022-03-01 18:30:49 +0900

    @query를 이용해 인터페이스에서 바로 쿼리문을 작성

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,39 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    //인터페이스 메서드에 바로 작성할수있음
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int
+            age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();  // 값 하나 조회
+
+
+    //dto로 조회 new 꼭사용 study.datajpa.dto 이렇게 객체를 생성해서 반환하는 문법
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    // 컬렉션 조회 :names 에 리스트가 들어가고 해당 리스트에 해당하는 내용이 있으면 검색
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+
+    List<Member> findListByUsername(String username); // 컬렉션
+    Member findMemberByUsername(String username); //단건
+    Optional<Member> findOptionalByUsername(String username);// 단건 optional
+
+
 }
