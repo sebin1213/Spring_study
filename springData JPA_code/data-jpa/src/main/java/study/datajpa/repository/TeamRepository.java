f998e805c339b32ed26c6ccb8ee180c118c9a9c2
hope1213 <1213hope@gmail.com>
2022-03-01 18:31:05 +0900

    team 스프링 데이터 jpa

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,2 +1,7 @@
-package study.datajpa.repository;public interface TeamRepository {
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.datajpa.entity.Team;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
