67dec2c6213cb333d9696e8b74daa0e2caafced3
hope1213 <1213hope@gmail.com>
2022-03-05 23:01:20 +0900

    서블릿 어플리케이션

--- springMVC_code/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ springMVC_code/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,12 +2,12 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 자동으로 하위패키지 찾아서 서블릿을 등록해준다
 @SpringBootApplication
 public class ServletApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
