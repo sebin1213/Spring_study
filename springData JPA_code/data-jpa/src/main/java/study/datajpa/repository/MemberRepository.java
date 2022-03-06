f5f3f70b2ef4bf0026df31f16a0bddba965cbcf3
hope1213 <1213hope@gmail.com>
2022-03-06 23:41:02 +0900

    스프링 데이터jpa사용한 페이징, 슬라이스페이징

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -36,4 +39,17 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findOptionalByUsername(String username);// 단건 optional
 
 
+    Slice<Member> findSliceByAge(int age, Pageable pageable); // 슬라이스 페이징
+    Page<Member> findByAge(int age, Pageable pageable); // 페이징
+/** 실무에서는 잘 안사용..?
+    // total count 같이 모든 데이터를 가져오고 개수를 세는건 좀 성능이 느림 이런 total count는 성능최적화를 잘해야함
+    // 만약에 회원들의 팀을 조회하고 전체 개수를 구한다고 했을때 (모든회원이 팀을 가졌다 가정)
+    // 굳이 회원을 팀과 조인하고 그다음 전체개수를 구할 필요가 없음... 그냥 회원의 수를 세면 됨
+    // count 쿼리를 분리하는 방법 사용
+**/
+// 분리
+    @Query(value = "select m from Member m left join m.team t",
+            countQuery = "select count(m.username) from Member m")
+    Page<Member> findTestByAge(int age, Pageable pageable); // 페이징
 }
+
