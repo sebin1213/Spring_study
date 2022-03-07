d580b5fb25130109fc20da26b6cfbc98043ac316
hope1213 <1213hope@gmail.com>
2022-03-07 22:25:42 +0900

    no message

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
@@ -1,2 +1,10 @@
-package study.datajpa.repository;public interface MemberRepositoryCustom {
+package study.datajpa.repository;
+
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+// memberRepository 에 존재하는 모든기능을 구현할수는 없으니 정말 커스텀하고싶은기능 따로 인터페이스로 빼서 커스텀
+public interface MemberRepositoryCustom {
+    List<Member> findMemberCustom();
 }
