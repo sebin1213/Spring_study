44bf125a54fe7a07313420b8582d1b7b8f512859
hope1213 <1213hope@gmail.com>
2022-03-20 21:48:41 +0900

    등록자 수정자가 필요없을때 따로 클래스를 만들음

---
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -0,0 +1,16 @@
+package study.datajpa.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+
+//보통 등록수정일은 모두 필요함 근데 등록자 수정자 는 필요없는 경우도 있음 그래서 따로 만들어서 상속받아 사용
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
