62adcbc4215d06b534360e47aeaa94214e072f0b
hope1213 <1213hope@gmail.com>
2022-03-22 21:46:01 +0900

    오류해결

--- springData JPA_code/data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
+++ springData JPA_code/data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
@@ -12,21 +12,22 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+// 스프링데이터 jpa로 해결하는 법
 @EntityListeners(AuditingEntityListener.class) // 이벤트로 동작한다....????
 @MappedSuperclass
 @Getter
 public class BaseEntity extends BaseTimeEntity {
-//    @CreatedDate
-//    @Column(updatable = false)
-//    private LocalDateTime createdDate;
-//    @LastModifiedDate
-//    private LocalDateTime lastModifiedDate;
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
     // 시간이라는건 현재 시간을 넣으면 되는데 등록자라는건 어디에서 가져와서 등록을 할까....??
     //jpa애플리케이션에서 확인
-    @CreatedBy
-    @Column(updatable = false)
-    private String createdBy;
-    @LastModifiedBy
-    private String lastModifiedBy;
+//    @CreatedBy
+//    @Column(updatable = false)
+//    private String createdBy;
+//    @LastModifiedBy
+//    private String lastModifiedBy;
 }
