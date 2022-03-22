4a0390bb4b72a9885f0338942423fa0683df995a
hope1213 <1213hope@gmail.com>
2022-03-22 23:43:31 +0900

    필기 추가

--- springData JPA_code/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ springData JPA_code/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -56,7 +56,7 @@ public class MemberTest {
     public void JpaEventBaseEntity() throws Exception {
 //given
         Member member = new Member("member1");
-        memberRepository.save(member); //@PrePersist 발생
+        memberRepository.save(member); //@PrePersist 발생 (트렌젝션 어노테이션 없어도 동작함 save를 구현한 코드에 이미 트렌젝셔널이 붙어있어서 대신에 save하고나면 트렌젝션이 끝나기 때문에 영속성 컨텍스트에서 사라짐..?(osiv 공부)
         Thread.sleep(100); // 잠깐 쉬기(원래는 사용안함)
 
         member.setUsername("member2");
