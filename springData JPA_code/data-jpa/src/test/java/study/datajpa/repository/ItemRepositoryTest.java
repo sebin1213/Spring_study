ff7d5a66c63ec275e00659fc5856c7d30ce468cb
hope1213 <1213hope@gmail.com>
2022-03-23 00:16:36 +0900

    save 테스트

--- springData JPA_code/data-jpa/src/test/java/study/datajpa/repository/ItemRepositoryTest.java
+++ springData JPA_code/data-jpa/src/test/java/study/datajpa/repository/ItemRepositoryTest.java
@@ -1,4 +1,20 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.entity.Item;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+// save() persist merge 메서드 테스트
+@SpringBootTest
 class ItemRepositoryTest {
-  
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void save(){
+        Item item = new Item("A"); // id 값은 jpa에 persist 하면 그때 들어가는거
+        itemRepository.save(item); // 트렌젝션 없어도 잘됨
+    }
 }
