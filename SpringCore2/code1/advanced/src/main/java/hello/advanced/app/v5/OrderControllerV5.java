319b3ddbab7373237f53386f0ebeeda2a24a5f48
hope1213 <1213hope@gmail.com>
2022-07-08 17:48:11 +0900

    no message

---
+++ SpringCore2/code1/advanced/src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate traceTemplate;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace logTrace) {
+        this.orderService = orderService;
+        // TraceTemplate 이거로 주입받아도 상관없음 하지만 테스트할때 좀더 편리하게 하기위해 분리했음
+        this.traceTemplate = new TraceTemplate(logTrace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return traceTemplate.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+}
