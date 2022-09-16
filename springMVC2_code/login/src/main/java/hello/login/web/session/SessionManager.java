8ff812c9c1dc95ced1dd0dc4893d6660b4c2e7e6
sebin1213 <1213hope@gmail.com>
2022-09-16 23:30:09 +0900

    no message

---
+++ springMVC2_code/login/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,74 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션 관리
+ */
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId"; // 자주써서 그냥 상수로 넣음
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    /**
+     * 세션 생성
+     * sessionId 생성 (임의의 추정 불가능한 랜덤 값)
+     * 세션 저장소에 sessionId와 보관할 값 저장
+     * sessionId로 응답 쿠키를 생성하서 클라이언트에 전달
+     */
+    public void createSession(Object value, HttpServletResponse response) {
+
+        //세션 id를 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        //쿠키 생성
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+    }
+
+    /**
+     * 세션 조회
+     */
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        } // 쿠키가 존재할때 찾고자하는 쿠키이름과 같은 쿠키를 찾음
+
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+        // 배열스트림은 list.stream()  형태가 아니라 Arrays.stream(list) 와 같은 형태로 사용한다.
+
+    }
+
+}
