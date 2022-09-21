2f8f2275a7825fa89f5502985131cc2bdd7ec825
sebin1213 <1213hope@gmail.com>
2022-09-21 23:34:05 +0900

    어노테이션으로 검증할수 없는 부분을 eventvalidater를 통해 검증

--- springREST/API/src/main/java/Rest/API/events/EventValidator.java
+++ springREST/API/src/main/java/Rest/API/events/EventValidator.java
@@ -1,2 +1,27 @@
-package Rest.API.events;public class EventValidator {
+package Rest.API.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
+            errors.reject("wrongPrices", "Values fo prices are wrong"); // 에러 발생
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong"); // 에러발생
+        }
+
+        // TODO BeginEventDateTime
+        // TODO CloseEnrollmentDateTime
+    }
+
 }
