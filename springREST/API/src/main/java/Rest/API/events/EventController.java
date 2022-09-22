322bf402f6595626f9d432ad16bd9ed2ee16da8f
sebin1213 <1213hope@gmail.com>
2022-09-22 23:42:44 +0900

    no message

--- springREST/API/src/main/java/Rest/API/events/EventController.java
+++ springREST/API/src/main/java/Rest/API/events/EventController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.time.LocalDateTime;
 
@@ -17,6 +20,9 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE) // 모든 핸들러는 HAL_JSON라는 content 타입으로 응답을 보냄
 public class EventController {
+    public final EventRepository eventRepository;
+    public final EventValidator eventValidator;
+
 //    @PostMapping("/api/events")
 //    public ResponseEntity createEvent(){
 //        URI createUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri(); // 링크를 만들고 uri로 변경
@@ -32,7 +38,6 @@ public class EventController {
 //        return ResponseEntity.created(createUri).body(event);
 //        // linkTo 링크를 만듬
 //    }
-    public final EventRepository eventRepository;
 
 //    @PostMapping
 //    public ResponseEntity createEvent(@RequestBody Event event){
@@ -47,7 +52,23 @@ public class EventController {
      * modelmapper이용해 변환..? 하지만 속도는 더 느림 (별로같음)
      * **/
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto){
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
+        if (errors.hasErrors()){  // 바인딩할때 검증과 관련된 오류가 발생하면 동작
+            System.out.println("error............");
+            // return ResponseEntity.badRequest().body(errors);
+            // 에러메세지를 전송하기위해 body에 error메세지를 담아 보내자..!
+            // 하지만 불가능함... body에 담으면 error객체를 json으로 변경할수 없어서 불가능
+            // 왜?? 이벤트객체는 자바 bean스펙에 준수한 객체이기 때문에 objectmapper는 이 객체를 json형태로 기본 시리얼라이저인 bean시리얼라이저를 사용해 변환한다.
+            // 하지만 errors는 자바 bean스펙에 준수한 객체가 아님... 때문에 objectmapper를 이용해 json형태로 변경하고싶어도 bean시리얼라이저를 사용할때 에러가 발생해 변환할 수 없다.
+            // 왜 json으로 변환..? 맨위에 produces = MediaTypes.HAL_JSON_VALUE json형태로 보낼꺼라고 명시함
+
+            return ResponseEntity.badRequest().build();
+        }
+
+        eventValidator.validate(eventDto,errors);
+        if (errors.hasErrors()){  // 바인딩 외에 발생하면 잘못입력된 데이터가 존재하면 동작
+            return ResponseEntity.badRequest().build();
+        }
 
         Event event = Event.builder()
                 .name(eventDto.getName())
@@ -64,7 +85,7 @@ public class EventController {
 
         Event newEvent = eventRepository.save(event); // repository 저장
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri(); // 링크를 만들고 uri로 변경
-        return ResponseEntity.created(createUri).body(event);
+        return ResponseEntity.created(createUri).body(event); // created 응답을 보낼때는 항상 uri가 존재해야함
     }
 
 }
