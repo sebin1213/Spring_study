926084c56a61dda63bb711f6970dd8ca8078eb78
sebin1213 <73850629+sebin1213@users.noreply.github.com>
2023-03-15 21:32:30 +0900

    Update EventController.java

--- springREST/API/src/main/java/Rest/API/events/EventController.java
+++ springREST/API/src/main/java/Rest/API/events/EventController.java
@@ -1,7 +1,9 @@
 package Rest.API.events;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -60,8 +62,7 @@ public class EventController {
             // 하지만 불가능함... body에 담으면 error객체를 json으로 변경할수 없어서 불가능(json으로 변환하느 이유는 produces = MediaTypes.HAL_JSON_VALUE라고 명시했기때문)
             // 왜?? 내가 선언한 Event 객체는 자바 bean스펙에 준수한 객체이기 때문에 objectmapper는 이 객체를 json형태로 기본 시리얼라이저인 bean시리얼라이저를 사용해 변환한다.
             // 하지만 errors는 자바 bean스펙을 준수하고있는 객체가 아님... 때문에 objectmapper를 이용해 json형태로 변경하고싶어도 bean시리얼라이저를 사용할때 에러가 발생해 변환할 수 없다.
-            // 왜 json으로 변환..? 맨위에 produces = MediaTypes.HAL_JSON_VALUE json형태로 보낼꺼라고 명시함
-
+            // 때문에 ErrorsSerializer 라는 시리얼라이저를 만들어 Errors를 json으로 시리얼라이징할때 해당 클래스를 사용할 수 있도록 함
             return ResponseEntity.badRequest().body(errors);
         }
 
@@ -71,10 +72,22 @@ public class EventController {
         }
 
         Event event = eventDto.toEntity();
-
+        event.update();
         Event newEvent = eventRepository.save(event); // repository 저장
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri(); // 링크를 만들고 uri로 변경
-        return ResponseEntity.created(createUri).body(event); // created 응답을 보낼때는 항상 uri가 존재해야함
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withSelfRel()); //self랑 update랑 같은링크인데 기능만 다름
+        eventResource.add(selfLinkBuilder.withRel("update-event")); // 링크가 같아도 put메서드를 사용하기때문에 상관없음
+
+//        EntityModel<Event> eventEntityModel = EntityModel.of(event,
+//                selfLinkBuilder.slash(event.getId()).withSelfRel(),
+//                linkTo(EventController.class).withRel("query-events"),
+//                selfLinkBuilder.withRel("update-event")
+//        ); // 따로 클래스를 작성하지않고 이렇게 작성해도 됨
+
+        return ResponseEntity.created(createUri).body(eventResource); // created 응답을 보낼때는 항상 uri가 존재해야함
     }
 
 }
